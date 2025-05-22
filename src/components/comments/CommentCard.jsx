d9d66fd5a655d6e36075ce0d25ec30be497097eb
7doublem <mustafamoallim@gmail.com>
2025-05-22 01:43:42 +0100

    complete first draft delete comments

--- src/components/comments/CommentCard.jsx
+++ src/components/comments/CommentCard.jsx
@@ -1,6 +1,32 @@
 import VoteCard from "../VoteCard.jsx";
+import bin from "../../deleteIcon/bin.svg";
+import { useState } from "react";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+import { deleteCommentById } from "../../Api.js";
 
 function CommentCard({ comment }) {
+  const { user } = useContext(UserContext);
+  const [error, setError] = useState(null);
+  const [isDeletingComment, setIsDeletingComment] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+  const [message, setMessage] = useState("");
+
+  function deleteComment() {
+    setIsDeletingComment(true);
+    setError(null);
+    deleteCommentById(comment.comment_id)
+      .then(() => {
+        setIsDeleted(true);
+        setMessage("Comment deleted successfully!");
+      })
+      .catch(() => {
+        setError("Failed to delete. Please try again");
+      })
+      .finally(() => {
+        setIsDeletingComment(false);
+      });
+  }
   return (
     <div>
       <section>
@@ -12,10 +38,26 @@ function CommentCard({ comment }) {
           id={comment.comment_id}
           type="comment"
         />
+        {user?.username === comment.author && !isDeleted && (
+          <div>
+            <button>
+              <img
+                src={bin}
+                alt="Delete Comment"
+                width={24}
+                height={24}
+                onClick={deleteComment}
+                disabled={isDeletingComment}
+              />
+            </button>
+            
+          </div>
+        )}
+        {message && <p>{message}</p>}
+        {error && <p>{error}</p>}
       </section>
     </div>
   );
 }
-
 
 export default CommentCard;
