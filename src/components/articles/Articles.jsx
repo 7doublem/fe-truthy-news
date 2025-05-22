bd3e06b0ca6e4e8fa59abe287516a74cfc5f13f9
7doublem <mustafamoallim@gmail.com>
2025-05-22 13:42:18 +0100

    complete first draft sort articles

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,15 +1,18 @@
 import NavBar from "../navigation/NavBar";
 import { useState, useEffect } from "react";
-import { getAllArticles } from "../../Api";
+import { getSortedArticles } from "../../Api";
 import ArticleCard from "./ArticleCard";
+import SortArticles from "./SortArticles";
 
 function Articles() {
   const [articles, setArticles] = useState([]);
   const [isLoadingArticles, setisLoadingArticles] = useState(true);
+  const [sortBy, setSortBy] = useState("created_at");
+  const [order, setOrder] = useState("desc");
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    getAllArticles()
+    getSortedArticles(sortBy, order)
       .then((res) => {
         setArticles(res.data.articles);
         setisLoadingArticles(false);
@@ -18,7 +21,7 @@ function Articles() {
         setisLoadingArticles(false);
         setError(err);
       });
-  }, []);
+  }, [sortBy, order]);
 
   if (isLoadingArticles) {
     return <p>Loading articles..</p>;
@@ -31,6 +34,12 @@ function Articles() {
     <div>
       <NavBar />
       <h2>Articles</h2>
+      <SortArticles
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        order={order}
+        setOrder={setOrder}
+      />
       {articles.map((article) => (
         <ArticleCard key={article.article_id} article={article} />
       ))}
